3a6cdacd8377248e5c9c8af3b6aef3a0c533d2f3
Pradeep Kumar G <63275384+byronblaze-ml@users.noreply.github.com>
2021-04-20 12:46:37 +0530

    Final Commit

--- server/index.js
+++ server/index.js
@@ -14,14 +14,30 @@ const db = mysql.createConnection({
 });
 
 //UserAuth
-app.get('/login', (req, res) => {
-    db.query('SELECT * FROM players', (err, result) => {
-        if(err) {
-            console.log(err);
-        } else {
-            res.send(result);
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    db.query(
+        'SELECT * FROM users WHERE username = ?',
+        username,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+            }
+            if (results.length > 0) {
+                if (password == results[0].password) {
+                res.json({ loggedIn: true, username: username });
+                } else {
+                res.json({
+                    loggedIn: false,
+                    message: "Wrong username/password combo!",
+                });
+                }
+            } else {
+                res.json({ loggedIn: false, message: "User doesn't exist" });
+            }
         }
-    });
+  );
 });
 
 
@@ -29,12 +45,36 @@ app.get('/login', (req, res) => {
 
 //Insertion
 app.post('/create', (req, res) => {
+    const player_id = req.body.player_id;
     const name = req.body.name;
     const age = req.body.age;
     const country = req.body.country;
     const position = req.body.position;
 
-    db.query('INSERT INTO players (name, age, country, position) VALUES (?,?,?,?)', [name, age, country, position], 
+    db.query('INSERT INTO players (player_id, name, age, country, position) \
+        VALUES (?,?,?,?,?)', [player_id, name, age, country, position], 
+        (err, result) => {
+            
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send('Values Inserted');
+        }
+    });
+});
+
+//Insert to Player_Stats
+app.post('/createstats', (req, res) => {
+    const player_id = req.body.player_id;
+    const appearances = req.body.appearances;
+    const goals = req.body.goals;
+    const assists = req.body.assists;
+    const red_cards = req.body.red_cards;
+
+    db.query('INSERT INTO player_stats \
+    (player_id, appearances, goals, assists, red_cards) \
+    VALUES (?,?,?,?,?)', [player_id, appearances, goals, assists, red_cards], 
     (err, result) => {
         
     if (err) {
@@ -57,16 +97,76 @@ app.get('/players', (req, res) => {
     });
 });
 
-//Update
-//app.put('/update', (req, res) => {
-//    const id=req.body.id;
- //   db.query("UPDATE SET players ")
-//})
+// Display all players stats
+app.get('/playerstats', (req, res) => {
+    db.query(
+        'SELECT player_stats.player_id as player_id, \
+        name, appearances, goals, assists, red_cards\
+        FROM player_stats JOIN players on players.player_id=player_stats.player_id', 
+        (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
 
-// Delete
+//Update
+app.put('/update', (req, res) => {
+    const id=req.body.player_id;
+    const name=req.body.name;
+    const age=req.body.age;
+    const country=req.body.country;
+    const position=req.body.position;
+    db.query("UPDATE players SET name= ?, age = ?, country = ?, position = ? WHERE player_id = ?",
+        [name, age, country, position,id],
+        (err, result) => {
+            if(err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        }
+    );
+});
+
+app.put('/updatestats', (req, res) => {
+    const id=req.body.player_id;
+    const appearances=req.body.appearances;
+    const goals=req.body.goals;
+    const assists=req.body.assists;
+    const red_cards=req.body.red_cards;
+    db.query("UPDATE player_stats SET \
+        appearances= ?, goals = ?, assists = ?, red_cards = ? WHERE player_id = ?",
+        [appearances, goals, assists, red_cards,id],
+        (err, result) => {
+            if(err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        }
+    );
+});
+
+//Clear Logs
+app.delete('/clearlogs', (req, res) => {
+    //const id = req.params.id;
+    db.query("DELETE FROM logs", (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+            console.log('success');
+        }
+    });
+});
+
+// Delete PLayers
 app.delete('/delete/:id', (req, res) => {
     //const id = req.params.id;
-    db.query("DELETE FROM players WHERE id = ?", req.params.id, (err, result) => {
+    db.query("DELETE FROM players WHERE player_id = ?", req.params.id, (err, result) => {
         if (err) {
             console.log(err);
         } else {
@@ -77,6 +177,72 @@ app.delete('/delete/:id', (req, res) => {
 });
 
 
+//Delete Player Stats
+app.delete('/deletestats/:id', (req, res) => {
+    const id = req.params.id;
+    db.query("DELETE FROM player_stats WHERE player_id = ?", id, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+            console.log(id);
+        }
+    });
+});
+
+
 app.listen(3001, () => {
     console.log('Server running...');
+});
+
+//Logs
+app.get('/logs', (req, res) => {
+    
+    db.query('SELECT * FROM logs', (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+
+//Search by ID
+app.get('/searchid/', (req, res) => {
+    const {search} = req.query;
+    db.query(`select * from player_stats`,search, (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send(result[0]);
+            console.log(result[0]);
+        }
+    });
+});
+//Procedures
+//SearchbyName
+app.get('/searchname/', (req, res) => {
+    const {search} = req.query;
+    db.query(`call searchName(?)`,search, (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send(result[0]);
+            console.log(result[0]);
+        }
+    });
+});
+
+//SearchbyPosition
+app.get('/searchposition/', (req, res) => {
+    const {search} = req.query;
+    db.query(`call searchPosition(?)`,search, (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send(result[0]);
+            console.log(result[0]);
+        }
+    });
 });
--- server/routes/user.js
+++ server/routes/user.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 
-app.get('/login', (req, res) => {
+/*app.get('/login', (req, res) => {
     db.query('SELECT * FROM players', (err, result) => {
         if(err) {
             console.log(err);
@@ -13,22 +13,29 @@ app.get('/login', (req, res) => {
         }
     });
 });
-
+*/
 app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
     db.query('SELECT * FROM users WHERE username = ?', username, 
     (err, results) => {
-        if(err) {
-            console.log(err);
-        }
-        if(results) {
-            if(password== results.password){
-                res.send('You are logged in!');
-            } else {
-                res.send('Wrong username/password');
-            }
+      if (err) {
+        console.log(err);
+      }
+      if (results.length > 0) {
+        if (password == results[0].password) {
+          res.json({ loggedIn: true, username: username });
         } else {
+          res.json({
+            loggedIn: false,
+            message: "Wrong username/password combo!",
+          });
+        }
+      } else {
+        res.json({ loggedIn: false, message: "User doesn't exist" });
             res.send("User doesn't exist");
         }
-        res.send(results);
-    })
-})
+        
+    });
+});
